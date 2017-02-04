931b28133f16b3c6909c42a3b63bb8873a317019
Kyosee <609671961@qq.com>
2017-02-04 15:37:14 +0800

    重写主要JS结构不依赖jquery

--- js/store_map.js
+++ js/store_map.js
@@ -1,61 +1,132 @@
-/**
- * StoreMap
- * @author Kyosee
- * 2017-2-3 16:01:54
- */
+;( function (w) {
+    /**
+     * 全局配置信息
+     * @param   String  key     高德API KEY
+     * @param   String  tableid 高德云图ID
+     * @param   String  map     地图元素容器ID名称
+     */
+    var def_config = {
+        key : '',
+        tableid : '',
+        map : '#map'
+    }
 
-(function(w){
-    var def_option = {
-        key : '',                       //高德API Key
-        tableid : '',                   //云图tableid
-        map_selector : '#map',          //地图选择器名称
-        init_fun : function(){
-            this.importJS("http://webapi.amap.com/maps?v=1.3&key="+smap.config.key);
-        }
-    }
-    w.smap = {
-        option : def_option,
-        config : function(option){
-            this.option = $.extend({}, this.option , option);
-            this.option.init_fun();
-        },
-        create_map : function(){
-            layer.prompt({title: 'Please input your Map Name', formType: 0}, function(name, index){
-                layer.close(index);
-                if(!smap.config.key){
-                    layer.msg("please config your key");
-                }
-                $.ajax({
-                    url: 'http://yuntuapi.amap.com/datamanage/table/create',
-                    type: 'post',
-                    dataType: 'json',
-                    data: {
-                        key: smap.config.key,
-                        name : name
-                    },
-                    success:function(data){
-                        if(data.status != 1){
-                            layer.msg(data.info + " code : " + data.infocode);
-                        }else{
-                            smap.config.tableid = data.tableid;
-                            return data;
-                        }
+    /**
+     * 远程创建云图
+     * @param   String  name    云图名称
+     * @param   String  key     高德API KEY 默认不填读取全局配置的key
+     * @param   Boolen  key     高德API KEY 默认不填读取全局配置的key
+     * @method  init()  初始化请求创建云图 return 云图创建结果
+     */
+    var def_create = {
+        name : 'test',
+        key : '',
+        init : function () {
+            smap.tools.ajax({
+                url: 'http://yuntuapi.amap.com/datamanage/table/create',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                    key: this.key,
+                    name : this.name
+                },
+                success : function (data) {
+                    if(data.status !== 1) {
+                        this.success(data);
+                    }else{
+                        this.fail(data);
                     }
-                })
-            });
+                },
+                error : function (XMLHttpRequest, textStatus, errorThrown) {
+                    this.error(XMLHttpRequest, textStatus, errorThrown);
+                }
+            })
         },
-        /**
-         * 初始化创建地图
-         */
-        init_map : function(){
-            // var map = new AMap.Map("map", {
-            //         resizeEnable: true,
-            //         center: [116.397428, 39.90923],//地图中心点
-            //         zoom: 12 //地图显示的缩放级别
-            // });
+        success : function (data) {
+            console.success("create success : "+data.tableid);
         },
-        importJS : function(filename){
-            document.write('<script language=\"javascript\" src='+filename+'> <\/script>');
+        fail : function (data) {
+            console.error("create fail : "+data.info + " code : " + data.infocode)
+        },
+        error : function (XMLHttpRequest, textStatus, errorThrown) {
+            console.error(textStatus + errorThrown);
         }
     }
-}(window))
+
+    /**
+     * 主方法
+     */
+    w.smap = {
+        setting : def_config,
+        create_setting : def_create,
+        config : function (option) {
+            this.setting = this.tools.extend(this.setting, option);
+        },
+        create : function (option) {
+            this.create_setting.key = this.setting.key;
+            this.create_setting = this.tools.extend(this.create_setting , option);
+            this.create_setting.init();
+        },
+    }
+
+    /**
+     * 工具类 参考网络
+     */
+    w.smap.tools = {
+        extend : function (obj, obj2) {
+            for (var key in obj2) {
+                 obj[key] = obj2[key];
+            }
+            return obj;
+        },
+        ajax : function (obj){
+            obj = obj||{};
+            obj.type = (obj.type||'GET').toUpperCase();
+            obj.dataType = obj.dataType||'json';
+            var params = formatParams(obj.data);//参数格式化
+
+            //step1:兼容性创建对象
+            if(window.XMLHttpRequest){
+                var xhr = new XMLHttpRequest();
+            }
+            else{
+                var xhr = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+
+            //step4: 接收
+            xhr.onreadystatechange = function(){
+                if(xhr.readtState == 4){
+                    if(xhr.state>=200 && xhr.status<300){
+                        obj.success&&obj.success(xhr.responseText,xhr.responseXML);
+                    }
+                    else{
+                        obj.error&&obj.error(xhr.status);
+                    }
+                }
+            }
+
+            //step2 step3:连接 和 发送
+            if(obj.type == 'GET'){
+                xhr.open('GET',obj.url+'?'+params,true);
+                xhr.send(null);
+            }
+            else if(obj.type == 'POST'){
+                xhr.open('POST',obj.url,true);
+                //设置请求头，以表单形式提交数据
+                xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+                xhr.send(params);
+            }
+
+            //辅助函数，格式化参数
+            function formatParams(data){
+                var arr = [];
+                for(var name in data){
+                    arr.push(encodeURIComponent(name)+"="+encodeURIComponent(data[name]));
+                }
+                //设置随机数，防止缓存
+                arr.push("t="+Math.random());
+                return arr.join("&");
+            }
+        }
+    }
+})(window);
