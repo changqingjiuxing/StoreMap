fdd99aa376c79121ca3eea24a2de3b84d0a32c61
Kyosee <609671961@qq.com>
2017-02-05 20:27:34 +0800

    修改php文件

--- map.php
+++ map.php
@@ -1,5 +1,12 @@
 <?php
     require_once("./tools.php");
+
+    function __construct(){
+        $fun_name = $_G['fun'];
+        if (function_exists('fun_name'))
+            call_user_func($fun_name);
+    }
+
     function create_new_map(){
         $data['key'] = '8edbb813fca4cb8b0b82299a86cbafba';
         $data['name'] = 'asd';
